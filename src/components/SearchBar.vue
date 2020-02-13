d8d863e55752edf03da7111c139aca659935082c
Levi Donaldson <lcdbacanas@gmail.com>
2020-02-12 18:21:20 -0600

    Update SearchBar.vue

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -23,13 +23,13 @@ export default {
         font-size: 20pt;
         font-weight: bold;
         border: none;
-        border-bottom: 2px solid#fff;
+        border-bottom: 2px solid #fff;
         color: #fff;
         outline: none;
         text-align: center;
         background: transparent;
     }
-    @media screen and (min-width: 750px) {
+    @media screen and (min-width: 46.8rem) {
         input { padding-top: 5rem; }
     }
 </style>
